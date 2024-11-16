4a9648d32342469355205c482e9dd929d749dc3f
gustavo.g.neto@gmail.com <32483792+gustavogneto@users.noreply.github.com>
2024-11-16 12:35:24 -0300

    corrections in mapped tables and cofigurations ir properties arquives profile test

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -20,23 +20,27 @@ public class Game {
 	
 	@Column(name = "game_year")
 	private Integer year;
-	private String gender;
-	private String plataforms;
+	private String genre;
+	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {}
 	
-	public Game(long id, String title, Integer year, String gender, String plataforms, Double score, String imgUrl,
+	public Game(long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		
 		this.id = id;
 		this.title = title;
 		this.year = year;
-		this.gender = gender;
-		this.plataforms = plataforms;
+		this.genre = genre;
+		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -67,20 +71,20 @@ public class Game {
 		this.year = year;
 	}
 
-	public String getGender() {
-		return gender;
+	public String getgenre() {
+		return genre;
 	}
 
-	public void setGender(String gender) {
-		this.gender = gender;
+	public void setgenre(String genre) {
+		this.genre = genre;
 	}
 
-	public String getPlataforms() {
-		return plataforms;
+	public String getplatforms() {
+		return platforms;
 	}
 
-	public void setPlataforms(String plataforms) {
-		this.plataforms = plataforms;
+	public void setplatforms(String platforms) {
+		this.platforms = platforms;
 	}
 
 	public Double getScore() {
